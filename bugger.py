c030afd348fe5191023bdcc969d9f10657412d9a
Anton Lofgren <anton.lofgren@gmail.com>
2013-10-23 22:00:32 +0200

    add paging, when the window is too small to fit all of the output

--- bugger.py
+++ bugger.py
@@ -3,6 +3,17 @@ import mechanize
 from bug import Bug, BugNotFound
 import textwrap
 import argparse
+import pydoc
+
+class NoPaging(Exception): pass
+def windowsize():
+	from fcntl import ioctl
+	from termios import TIOCGWINSZ
+	from array import array
+	winsize = array("H", [0, 0, 0, 0] )
+	ioctl(sys.stdout.fileno(), TIOCGWINSZ, winsize)
+	return (winsize[1], winsize[0])
+
 class Bugger(object):
 	def __init__(self, url):
 		self.url = url
@@ -21,13 +32,23 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 	url = args.url
 	bug_id = args.id
+	output = ""
 	try:
 		bug = Bugger(url).bug(bug_id)
-		print bug.summary
-		print
-		print textwrap.fill("Description: " + bug.description)
-		print
-		print textwrap.fill("Additional Information: " + bug.additional_information)
+		output += bug.summary
+		output += "\n"
+		output += "Description: " + bug.description
+		output += "\n"
+		output += "Additional Information: " + bug.additional_information
 	except BugNotFound:
-		print "Sorry, that bug doesn't exist."
-	
+		output = "Sorry, that bug doesn't exist."
+
+	try:
+		width, height = windowsize()
+		if output.count("\n") > height:
+			pydoc.pager(output)
+		else:
+			raise NoPaging
+	except (IOError, NoPaging):
+		print(output)
+		pass
