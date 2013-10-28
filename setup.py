790e53ec1ecb14d640d4afc10b0c9067c5f93543
Anton Lofgren <alofgren@op5.com>
2013-10-28 09:09:43 +0100

    add setuptools/setup.py support
    Signed-off-by: Anton Lofgren <alofgren@op5.com>

---
+++ setup.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+from setuptools import setup, find_packages
+from subprocess import check_output
+
+def get_version():
+	tag = check_output(["git", "tag"])
+	hash = check_output(["git", "log", "-1", "--format=%H"])[:10]
+	return "%s-p%s" % (tag, hash)
+
+setup(
+		name = "bugger",
+		version = get_version(),
+		packages = find_packages(),
+		author = "Anton Löfgren",
+		author_email = "anton.lofgren@gmail.com",
+		license = "BSD (2-clause)",
+		keywords = "mantis mechanize bugger bugs bug tracker issue issues",
+		url = "https://github.com/catharsis/bugger",
+		install_requires = open('requirements.txt').read().split('\n')
+		)
